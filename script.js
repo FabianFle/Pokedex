f7bbf4d3e1d37d4ebb0b3df52f66b3251dd206e3
FabianFle <fabianflegler1@yahoo.de>
2022-07-22 11:52:02 +0200

    bugup save

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let allpokemons = [];
 let currentPokemon;
 let startNumber = 1;
 let pokemonNumbers = 100;
@@ -8,7 +9,8 @@ async function loadPokemon() {
         let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         let res = await fetch(url);
         currentPokemon = await res.json();
-        console.log(currentPokemon);
+        allpokemons.push(currentPokemon);
+        // console.log(currentPokemon);
         renderPokemonCard(currentPokemon, i);                
     }
 }
@@ -27,7 +29,7 @@ function loadPokeClasses(i) {
     for (let c = 0; c < currentPokemon['types'].length; c++) {
         let pokeClass = firstLetter(currentPokemon['types'][c]['type']['name']);
         if (c == 0) {
-            document.getElementById(`classes${i}`).innerHTML += `<span class="pokemon-card-class">${pokeClass}</span>`;
+            document.getElementById(`classes${i}`).innerHTML += `<span class="pokemon-card-class card-${pokeClass}">${pokeClass}</span>`;
             document.getElementById(`pokemonCard${i}`).classList.add(`card-${pokeClass}`);
         } else {
             document.getElementById(`classes${i}`).innerHTML += `<span class="pokemon-card-class card-${pokeClass}">${pokeClass}</span>`;
@@ -37,21 +39,24 @@ function loadPokeClasses(i) {
 
 
 function searchPokemon() {
-    const search = document.getElementById("search-field").value.toLowerCase();
+    let search = document.getElementById("inputfield").value;
+    search = search.toLowerCase();
+    console.log(search);
     for (let i = 1; i < pokemonNumbers; i++) {
-        let pokeCard = document.getElementById(`pokemonCard${i}`);
-        let pokeName = document.getElementById(`pokemonName${i}`).innerHTML;
-        let pokeId = document.getElementById(`pokemonId${i}`).innerHTML;
-        if (!pokeName.includes(search) && !pokeId.includes(search)) {
-            pokeCard.classList.add('d-none');
+        let pokemonCard = document.getElementById(`pokemonCard${i}`);
+        let pokemonName = document.getElementById(`pokemonName${i}`).innerHTML;
+        let pokemonId = document.getElementById(`pokemonId${i}`).innerHTML;
+        if (!pokemonName.includes(search) || !pokemonId.includes(search)) {
+            pokemonCard.classList.add('d-none');
         }
         else {
-            if (!pokeCard.classList.contains('d-none')) {
-                pokeCard.classList.remove('d-none');
+            if (pokemonCard.classList.contains('d-none')) {
+                pokemonCard.classList.remove('d-none');
             }
         }
     }
 }
+
 
 function firstLetter(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
--- templates.js
+++ templates.js
@@ -2,6 +2,7 @@ function pokeCards(i, pokemonName, currentPokemon, pokeImg) {
     return /*html*/`
 
 <div onclick="openPokemon()" class="pokemon-card" id="pokemonCard${i}">
+    <div class="pokeBackground">        
         <div class="info-top">
             <h2 class="info-top-id" id="pokemonName${i}">${pokemonName}</h2> 
             <h2 class="info-top-id" id="pokemonId${i}">#${currentPokemon['id']}</h2>
@@ -9,8 +10,9 @@ function pokeCards(i, pokemonName, currentPokemon, pokeImg) {
         <div class="info-bottom">
             <div id="classes${i}"></div>
         </div>
-        <img src="${pokeImg}">        
-    </div>
+        <img src="${pokeImg}">   
+    </div>     
+</div>
     `;
 }
 
